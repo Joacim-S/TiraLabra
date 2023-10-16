4898608337cd47b506c9cf9620f75b7e0b348c3a
Joacim-S <45919018+Joacim-S@users.noreply.github.com>
2023-10-16 14:23:40 +0300

    Toteutettu lz78 pakkaus binääridataksi

--- src/lz78.py
+++ src/lz78.py
@@ -1,42 +1,45 @@
 '''LZ78 algoritmilla pakkaus ja myöhemmin purku'''
-def compress_too_slow(text=str):
-    '''Koodaa syötetyn merkkijonon LZ78 algoritmilla ja palauttaa koodatun jonon,
-    jossa mukana EOF merkkinä toimiva "$"'''
-    text += '$'
-    output = ''
-    dictionary = [(0,'')]
+def compress(text):
+    '''Koodaa syötetyn merkkijonon LZ78 algoritmilla'''
     last_match = 0
-    i = 0
-    while i < len(text):
-        for j, item in enumerate(dictionary):
-            if item == (last_match, text[i]):
-                last_match = j
-                i += 1
-        dictionary.append((last_match, text[i]))
-        output += f'{last_match}{text[i]}'
-        last_match = 0
-        i+=1
-    return output
-
-def compress(text=str):
-    '''Koodaa syötetyn merkkijonon LZ78 algoritmilla ja palauttaa koodatun jonon,
-    jossa mukana EOF merkkinä toimiva "$"'''
-    text += '$'
-    output = ''
-    dictionary = {(0, '') : 0}
-    last_match = 0
-    next_index = 1
-    i = 0
+    next_index = 2
+    i = 1
+    dictionary = {(0, '') : 0, (0, text[0]) : 1}
+    output = [bin(ord(text[0]))[2:].zfill(8)]
+    bits = 1
+    rounds = 0
+    round_limit = 1
+    added = False
+    print('Luodaan sanakirjaa...')
     while i < len(text):
         key = (last_match, text[i])
         if key in dictionary:
             last_match = dictionary[key]
             i += 1
+            added = False
 
         else:
+            added = True
             dictionary[last_match, text[i]] = next_index
             next_index += 1
-            output += f'{last_match}{text[i]}'
+            output.append(f'{last_match:0{bits}b}{bin(ord(text[i]))[2:].zfill(8)}')
             last_match = 0
             i+=1
-    return output
+            rounds += 1
+            if rounds == round_limit:
+                bits += 1
+                round_limit *= 2
+                rounds = 0
+    if not added:
+        output.append(f'{last_match:0{bits}b}')
+
+    print('Muutetaan tavuiksi...')
+    output_string = ''.join(output)
+    output_len = len(output_string)
+    output_byte_list = []
+    for i in range(0, output_len, 8):
+        output_byte_list.append(int(output_string[i:i+8], 2))
+    
+    result = bytearray(output_byte_list)
+
+    return result
--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 '''Moduuli, jolla algoritmeja käytetään.'''
 import lz78
 from datetime import datetime
-start = datetime.now()
-with open('input.txt') as input_file:
-    input_string = input_file.read()
 
-output_string = lz78.compress(input_string)
-with open('output.txt','w') as output_file:
-    output_file.write(output_string)
-print(datetime.now()-start)
+def run_lz78():
+    start = datetime.now()
+    with open('input.txt') as input_file:
+        input_string = input_file.read()
+
+    output_string = lz78.compress(input_string)
+    print('Kirjoitetaan...')
+    with open('output.bin','wb') as output_file:
+        output_file.write(output_string)
+    print(datetime.now()-start)
+
+run_lz78()
